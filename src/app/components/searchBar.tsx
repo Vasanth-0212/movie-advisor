74e5df503d5023510079a05e2af5966348935297
Vasanth-0212 <vasanthathuluri02@gmail.com>
2025-01-05 21:51:37 +0530

    vercel issues

--- src/app/components/searchBar.tsx
+++ src/app/components/searchBar.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { get } from "http";
+import React, { useEffect, useState } from "react";
 import DefaultList from "./defaultList";
-import React, { use, useEffect, useState } from "react";
 import {
     Sheet,
     SheetContent,
@@ -9,25 +8,32 @@ import {
     SheetHeader,
     SheetTitle,
     SheetTrigger,
-} from "../../components/ui/sheet"
+} from "../../components/ui/sheet";
 import useStore from "../lib/useStockStore";
-import { ScrollArea } from "../../components/ui/scroll-area"
 
-
-interface MovieList {
-    wishlist: any[];
-    setWishList: (wishlist: any[]) => void;
+// Define the Movie interface
+interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    vote_average: number;
+    overview: string;
 }
 
-const searchBar = () => {
+interface MovieList {
+    wishlist: Movie[];
+    setWishList: (wishlist: Movie[]) => void;
+}
 
-    const [movies, setMovies] = useState([]);
-    const [search, setSearch] = useState("");
-    const [error, setError] = useState("");
+const SearchBar = () => {
+    const [movies, setMovies] = useState<Movie[]>([]); // Use Movie[] type for movies
+    const [search, setSearch] = useState<string>(""); // Type search as string
+    const [error, setError] = useState<string>(""); // Type error as string
 
     const wishlist = useStore((state: MovieList) => state.wishlist);
     const setWishList = useStore((state: MovieList) => state.setWishList);
 
+    // Fetch trending movies
     async function getMovies() {
         try {
             const response = await fetch(
@@ -42,7 +48,7 @@ const searchBar = () => {
 
             if (response.ok) {
                 const data = await response.json();
-                setMovies(data.results);
+                setMovies(data.results); // Use Movie[] type for setMovies
             } else {
                 console.error("Failed to fetch movies.");
             }
@@ -51,7 +57,8 @@ const searchBar = () => {
         }
     }
 
-    async function searchMovie(query: any) {
+    // Search for movies
+    async function searchMovie(query: string) { // Type query as string
         try {
             const response = await fetch(
                 `https://api.themoviedb.org/3/search/movie?api_key=f43ec82a5f24fe6190891894b7436c7a&query=${query}`
@@ -59,31 +66,33 @@ const searchBar = () => {
 
             if (response.ok) {
                 const data = await response.json();
-                setMovies(data.results);
-                setError("");
+                setMovies(data.results); // Use Movie[] type for setMovies
+                setError(""); // Clear error if search is successful
             } else {
                 setError("An error occurred while fetching movies.");
-                setMovies([]);
+                setMovies([]); // Reset movies in case of error
             }
         } catch (error) {
             setError("An error occurred while fetching movies.");
-            setMovies([]);
+            setMovies([]); // Reset movies in case of error
         }
     }
 
-    const handleSearch = (e: any) => {
+    // Handle search form submission
+    const handleSearch = (e: React.FormEvent) => { // Type e as React.FormEvent
         e.preventDefault();
         searchMovie(search.trim());
-    }
+    };
 
-    const handleRemoveFromWishlist = (movie: any) => {
+    // Handle removing a movie from the wishlist
+    const handleRemoveFromWishlist = (movie: Movie) => { // Type movie as Movie
         const updatedWishlist = wishlist.filter((m) => m.id !== movie.id);
         setWishList(updatedWishlist);
-    }
+    };
 
     useEffect(() => {
         console.log(wishlist);
-    }, [wishlist])
+    }, [wishlist]);
 
     useEffect(() => {
         if (search === "") {
@@ -91,16 +100,17 @@ const searchBar = () => {
         }
     }, [search]);
 
-
-
     return (
-        <div className="">
+        <div>
             <div className="flex flex-row justify-between items-center p-2 bg-blue-200 h-20 w-full">
                 <div className="flex space-x-4 ml-4">
                     <h1 className="text-3xl md:text-5xl font-extrabold font-sans text-blue-500">Movie Advisor</h1>
                     <form className="mt-2">
                         <div className="pt-2 space-x-2">
-                            <input type="text" placeholder="Search for a movie" className="bg-white border h-8 w-full md:w-48 rounded-md p-3 focus:outline-none border-blue-400"
+                            <input
+                                type="text"
+                                placeholder="Search for a movie"
+                                className="bg-white border h-8 w-full md:w-48 rounded-md p-3 focus:outline-none border-blue-400"
                                 value={search}
                                 onChange={(e) => setSearch(e.target.value)}
                             />
@@ -116,37 +126,39 @@ const searchBar = () => {
                 <div className="pt-2">
                     <Sheet>
                         <div className="p-2">
-                            <SheetTrigger className="h10 w-20 p-1 text-white bg-blue-500 font-medium border border-blue-500 rounded-lg">WishList</SheetTrigger>
+                            <SheetTrigger className="h10 w-20 p-1 text-white bg-blue-500 font-medium border border-blue-500 rounded-lg">
+                                WishList
+                            </SheetTrigger>
                         </div>
                         <SheetContent>
                             <SheetHeader>
                                 <SheetTitle>My WishList</SheetTitle>
-                                <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
-                                    <SheetDescription>
-                                        {wishlist.map((movie: any) => {
-                                            return (
-                                                <div key={movie.title} className="mt-2 bg-white shadow-2xl rounded-lg border border-black overflow-hidden">
-                                                    <div className="p-4 space-y-3">
-                                                        <h1 className="text-2xl font-bold text-gray-800">{movie.title}</h1>
-                                                        <p className="text-sm font-semibold text-gray-600">Rating: {movie.vote_average}</p>
-                                                        <button className="text-white bg-red-500 text-xl h-10 w-full rounded-xl font-bold"
-                                                            onClick={() => handleRemoveFromWishlist(movie)}
-                                                        >Remove from WishList</button>
-                                                    </div>
+                                <SheetDescription>
+                                    {wishlist.map((movie) => {
+                                        return (
+                                            <div key={movie.id} className="mt-2 bg-white shadow-2xl rounded-lg border border-black overflow-hidden">
+                                                <div className="p-4 space-y-3">
+                                                    <h1 className="text-2xl font-bold text-gray-800">{movie.title}</h1>
+                                                    <p className="text-sm font-semibold text-gray-600">Rating: {movie.vote_average}</p>
+                                                    <button
+                                                        className="text-white bg-red-500 text-xl h-10 w-full rounded-xl font-bold"
+                                                        onClick={() => handleRemoveFromWishlist(movie)}
+                                                    >
+                                                        Remove from WishList
+                                                    </button>
                                                 </div>
-                                            )
-                                        })}
-                                    </SheetDescription>
-                                </ScrollArea>
+                                            </div>
+                                        );
+                                    })}
+                                </SheetDescription>
                             </SheetHeader>
                         </SheetContent>
                     </Sheet>
                 </div>
             </div>
             <DefaultList movies={movies} />
-           
         </div>
-    )
-}
+    );
+};
 
-export default searchBar;
+export default SearchBar;
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,32 +1,25 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { cva, type VariantProps } from "class-variance-authority"
-import { X } from "lucide-react"
+import * as React from "react";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
+import { cva, type VariantProps } from "class-variance-authority";
+import { X } from "lucide-react";
 
-
-const Sheet = SheetPrimitive.Root
-
-const SheetTrigger = SheetPrimitive.Trigger
-
-const SheetClose = SheetPrimitive.Close
-
-const SheetPortal = SheetPrimitive.Portal
-
+const Sheet = SheetPrimitive.Root;
+const SheetTrigger = SheetPrimitive.Trigger;
+const SheetClose = SheetPrimitive.Close;
+const SheetPortal = SheetPrimitive.Portal;
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
->(({ className, ...props }, ref) => (
+>(({ ...props }, ref) => (
   <SheetPrimitive.Overlay
-    className={
-      `fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0`
-    }
+    className="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
     {...props}
     ref={ref}
   />
-))
-SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
+));
+SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
   "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
@@ -36,16 +29,17 @@ const sheetVariants = cva(
         top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
+        left:
+          "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
         right:
-          "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
       },
     },
     defaultVariants: {
       side: "right",
     },
   }
-)
+);
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
@@ -54,14 +48,10 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
+>(({ side = "right", children, ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
-    <SheetPrimitive.Content
-      ref={ref}
-      className={sheetVariants({ side })}
-      {...props}
-    >
+    <SheetPrimitive.Content ref={ref} className={sheetVariants({ side })} {...props}>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
@@ -69,57 +59,34 @@ const SheetContent = React.forwardRef<
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
   </SheetPortal>
-))
-SheetContent.displayName = SheetPrimitive.Content.displayName
+));
+SheetContent.displayName = SheetPrimitive.Content.displayName;
 
-const SheetHeader = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={
-      `flex flex-col space-y-2 text-center sm:text-left`}
-    {...props}
-  />
-)
-SheetHeader.displayName = "SheetHeader"
+const SheetHeader = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className="flex flex-col space-y-2 text-center sm:text-left" {...props} />
+);
+SheetHeader.displayName = "SheetHeader";
 
-const SheetFooter = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2"
-    }
-    {...props}
-  />
-)
-SheetFooter.displayName = "SheetFooter"
+const SheetFooter = ({ ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className="flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2" {...props} />
+);
+SheetFooter.displayName = "SheetFooter";
 
 const SheetTitle = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Title
-    ref={ref}
-    className={`text-lg font-semibold text-foreground`}
-    {...props}
-  />
-))
-SheetTitle.displayName = SheetPrimitive.Title.displayName
+>(({ ...props }, ref) => (
+  <SheetPrimitive.Title ref={ref} className="text-lg font-semibold text-foreground" {...props} />
+));
+SheetTitle.displayName = SheetPrimitive.Title.displayName;
 
 const SheetDescription = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Description
-    ref={ref}
-    className={`text-sm text-muted-foreground`}
-    {...props}
-  />
-))
-SheetDescription.displayName = SheetPrimitive.Description.displayName
+>(({ ...props }, ref) => (
+  <SheetPrimitive.Description ref={ref} className="text-sm text-muted-foreground" {...props} />
+));
+SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
   Sheet,
@@ -132,4 +99,4 @@ export {
   SheetFooter,
   SheetTitle,
   SheetDescription,
-}
+};
